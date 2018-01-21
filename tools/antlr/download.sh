13208bd8b99bf41ba848d133907d8c4a744abf0e
ArthurSonzogni <Arthur.Sonzogni@grenoble-inp.org>
2018-01-21 14:44:49 +0100

    Add download.sh

---
+++ tools/antlr/download.sh
@@ -0,0 +1,12 @@
+cd "$(dirname "$0")"
+top=$(pwd)
+
+mkdir -p cpp_runtime
+
+## Download ANTLR and the CPP runtime
+curl http://www.antlr.org/download/antlr-4.7.1-complete.jar -o antlr.jar
+curl http://www.antlr.org/download/antlr4-cpp-runtime-4.7.1-source.zip -o cpp_runtime/archive.zip
+
+## Unzip cpp runtime
+cd cpp_runtime
+unzip archive.zip
